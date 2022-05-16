4030656551fd663bc8715d20088e503bd744a6b9
Mayur Ghodke <mayurghodke111@gmail.com>
2022-05-16 15:16:47 +0530

    myproject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo_app';
+  title = 'To do ';
+  constructor() {
+    
+  }
 }
